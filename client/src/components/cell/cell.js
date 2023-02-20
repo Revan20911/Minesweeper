d897604b95dbe908c954d4980967ac0170f7c02d
Chris Merrill <revan2091@live.com>
2023-02-19 20:03:40 -0500

    edited checks for prop values and change the image accordingly

--- client/src/components/cell/cell.js
+++ client/src/components/cell/cell.js
@@ -2,28 +2,42 @@ import React from "react";
 import mine from "../assets/exploded_bomb.png";
 import flag from "../assets/flagged_bomb.png";
 import closed from "../assets/closed.png";
+import open from "../assets/open.png";
 
 export default class Cell extends React.Component{
-    getValue(){
+    getImage(){
+
 
         if(this.props.value.neighbour === 0 ){
-            return <img src={closed} alt="" />;
-        }
 
-        if(this.props.value.isFlagged == true){
-            return <img src={flag} alt=""/>
-        }
+            if(this.props.value.isOpen != false){
+
+                if(this.props.value.isMine == true){
+                    return <img src={mine} alt=""/>
+                }
+
+                if(this.props.value.isMine != true){
+                    return <img src={open}/>
+                }
+            }
+
+            }
+
+            if(this.props.value.isOpen == false){
+                return <img src={closed} alt=""/>
+            }
+            
+        
     }
 
     render(){
         let className = "cell";
 
-        console.log(this.props.value);
-        
+        console.log(this.props.value.isOpen)
 
         return(
-            <div  className={className} >
-                {this.getValue()}
+            <div  className={className} onClick={this.props.onClick} >
+                {this.getImage()}
             </div>
         );
     }
